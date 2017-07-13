709a290965806e30eef58b9e21f93e900131d45d
thylakoids <liyulongsjtu@hotmail.com>
2017-07-13 15:19:20 +0800

    log

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from api import *
+import sys
 app=Flask(__name__,static_url_path='/static')
 #data=getdata()
 #data={'nodes':pd.read_excel('nodes1.xlsx'),'edges':pd.read_csv('edges1.csv')}
@@ -7,11 +8,13 @@ app=Flask(__name__,static_url_path='/static')
 seednode="[{'id':21685086,'label':216850}]"
 @app.route('/')
 def root():
-	return render_template('index.html',nodes=seednode,edges='[]')
+	return render_template('index.html',nodes=[],edges='[]')
 @app.route('/pubmed/<pmid>')
 def getnode(pmid):
 	title=get_summary(pmid)['Title']
 	citedpmids=get_citation_id(pmid)
 	return jsonify({'title':title,'citedpmids':citedpmids})
 if __name__=='__main__':
-	app.run(debug=True,threaded=True,port=4000)
+	import logging
+    	logging.basicConfig(filename='flask.log',level=logging.DEBUG)
+	app.run(debug=False,threaded=True,port=4000,host='0.0.0.0')
